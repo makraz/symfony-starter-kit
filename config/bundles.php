3c6650a2743c5730a24d15e47d31b6375a25c0e7
Ayoub Nait Lamine <aynaitlamine.dev@gmail.com>
2025-01-04 10:41:42 +0100

    Add DoctrineTestBundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
