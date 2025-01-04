2ae9ca55feccd6356a0eb53a3b786cbbd401ba2c
Ayoub Nait Lamine <aynaitlamine.dev@gmail.com>
2025-01-04 10:38:12 +0100

    Add ZenstruckFoundryBundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
